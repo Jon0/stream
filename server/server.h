9a152a0ab30e21fdbe61bd67205166da8ff7633b
jon0 <jono4728@gmail.com>
2015-01-11 21:11:22 +1300

    clean up

--- server/server.h
+++ server/server.h
@@ -8,9 +8,10 @@
 
 #include "session.h"
 
-using boost::asio::ip::tcp;
-
 namespace io {
+
+using boost::asio::ip::tcp;
+using boost::asio::ssl::context;	
 
 class server {
 public:
@@ -19,17 +20,19 @@ public:
 		io_service(ios),
 		root_dir(root),
 		acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-		context_(io_service, boost::asio::ssl::context::sslv23) {
+		context_(io_service, context::sslv23) {
 
-		context_.set_options(boost::asio::ssl::context::default_workarounds
-								| boost::asio::ssl::context::no_sslv2
-								| boost::asio::ssl::context::single_dh_use);
+		context_.set_options(context::default_workarounds
+								| context::no_sslv2
+								| context::single_dh_use);
+
 		context_.set_password_callback(
-			[](std::size_t max_length,  boost::asio::ssl::context::password_purpose purpose) -> std::string {
+			[](std::size_t max_length,  context::password_purpose purpose) -> std::string {
 				return "test"; // very secure
 			});
+		
 		context_.use_certificate_chain_file("server.crt");
-		context_.use_private_key_file("server.key", boost::asio::ssl::context::pem);
+		context_.use_private_key_file("server.key", context::pem);
 		context_.use_tmp_dh_file("dh512.pem");
 
 		// start accepting requests
@@ -68,7 +71,7 @@ private:
 	std::function<void(str_map)> update_function;
 
 	tcp::acceptor acceptor_;
-	boost::asio::ssl::context context_;
+	context context_;
 
 
 	// todo automatically remove completed sessions
